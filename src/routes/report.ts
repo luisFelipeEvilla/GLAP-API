5546ff3b25d8d831d9db7c2578534d646a8caf6d
Luis Felipe Evilla <luisevilla588@gmail.com>
2022-09-23 23:01:15 -0500

    update and add reports

--- src/routes/report.ts
+++ src/routes/report.ts
@@ -1,9 +1,22 @@
 import express, { Request, Response } from 'express';
 import { Error as mongooseError } from "mongoose";
-import { createReport, deleteReports } from '../controllers/reportController';
+import { createReport, deleteReports, getReports, updateReports } from '../controllers/reportController';
 import { Report } from '../models/reportModel';
 
 const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const result = await getReports({});
+
+        res.json(result);
+    } catch (error) {
+        if (error instanceof mongooseError ) return  res.json(error).status(400)
+        if (error instanceof Error) return res.json({error: error.message}).status(500);
+
+        console.error(error);
+    }
+})
 
 router.post('/', async (req: Request, res: Response) => {
     const {...report} = req.body as Report;
@@ -37,4 +50,21 @@ router.delete('/:_id', async (req, res) => {
     }
 })
 
+router.patch('/:_id', async (req, res) => {
+    const { _id } = req.params;
+    const { ...report } = req.body as Report;
+
+    if (!_id || _id === null) return res.json({ error: 'Should pass a _id parameter'});
+
+    try {
+        const result = await updateReports({ _id}, report );
+
+        res.json(result).status(200);
+    } catch (error) {
+        if (error instanceof mongooseError ) return  res.json(error).status(400)
+        if (error instanceof Error) return res.json({error: error.message}).status(500);
+
+        console.error(error);
+    }
+})
 export default router;
